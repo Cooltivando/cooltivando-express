ac9dbd535569c181d485709e312e2e00f2b6b699
Rodrigo Reis <rodrigo.monney@gmail.com>
2020-01-12 07:02:01 -0300

    chore: create database migrations

---
+++ src/database/migrations/20200111204149-create-orders.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      message: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('orders');
+  },
+};
