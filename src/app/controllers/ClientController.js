8650b130f8168d5aa71b470bce2b62b17cba590b
Rodrigo Reis <rodrigo.monney@gmail.com>
2020-01-13 07:48:27 -0300

    chore: add validation for ClientController

--- src/app/controllers/ClientController.js
+++ src/app/controllers/ClientController.js
@@ -1,7 +1,23 @@
+import * as Yup from 'yup';
 import Client from '../models/Client';
 
 class ClientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      phone: Yup.string().required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const clientExists = await Client.findOne({
       where: { email: req.body.email },
     });
@@ -21,13 +37,34 @@ class ClientController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      phone: Yup.string(),
+      oldPassword: Yup.string()
+        .required()
+        .min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const { email, oldPassword } = req.body;
 
     const client = await Client.findByPk(req.user.id);
 
     if (email !== client.email) {
       const clientExists = await Client.findOne({
-        where: { email: req.body.email },
+        where: { email },
       });
 
       if (clientExists) {
@@ -35,11 +72,18 @@ class ClientController {
       }
     }
 
-    if (!(await client.checkPassword(oldPassword))) {
+    if (oldPassword && !(await client.checkPassword(oldPassword))) {
       return res.status(401).json({ error: 'Password does not match.' });
     }
 
-    return res.json();
+    const { id, name, phone } = await client.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      phone,
+    });
   }
 }
 
