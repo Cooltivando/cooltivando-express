d6e41a28ff0481973daea940499a537b3f1b6f0a
Rodrigo Reis <rodrigo.monney@gmail.com>
2020-01-13 07:48:46 -0300

    chore: add validation for OrderController

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,7 +1,17 @@
+import * as Yup from 'yup';
+
 import Order from '../models/Order';
 
 class OrderController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      message: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const { id, message } = await Order.create(req.body);
 
     return res.json({
@@ -10,7 +20,7 @@ class OrderController {
     });
   }
 
-  async update(req, res){
+  async update(req, res) {
     return res.json();
   }
 }
