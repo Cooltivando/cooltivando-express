28eda6b8760754309e90c711bec97e8695197cb7
Rodrigo Reis <rodrigo.monney@gmail.com>
2020-01-14 23:47:23 -0300

    chore: create /files route to handle file uploads for orders

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import ClientController from './app/controllers/ClientController';
 import CooperativeController from './app/controllers/CooperativeController';
 import OrderController from './app/controllers/OrderController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -21,5 +25,7 @@ routes.put('/cooperatives', CooperativeController.update);
 
 routes.post('/orders', OrderController.store);
 routes.put('/orders', OrderController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
