247767c3a927d81c64be76fb0aebf8baad0dc727
Rodrigo Reis <rodrigo.monney@gmail.com>
2020-01-14 23:48:09 -0300

    fix: add file_id validation using Yup

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -6,17 +6,19 @@ class OrderController {
   async store(req, res) {
     const schema = Yup.object().shape({
       message: Yup.string(),
+      file_id: Yup.number(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails.' });
     }
 
-    const { id, message } = await Order.create(req.body);
+    const { id, message, file_id } = await Order.create(req.body);
 
     return res.json({
       id,
       message,
+      file_id,
     });
   }
 
